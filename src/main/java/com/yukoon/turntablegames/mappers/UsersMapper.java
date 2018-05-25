a20a3589fb204385e6cb2dead327913e567cbda2
a5928497 <61722615@qq.com>
2018-05-26 00:41:37 +0800

    developing

--- src/main/java/com/yukoon/turntablegames/mappers/UsersMapper.java
+++ src/main/java/com/yukoon/turntablegames/mappers/UsersMapper.java
@@ -2,9 +2,13 @@ package com.yukoon.turntablegames.mappers;
 
 import com.yukoon.turntablegames.entities.User;
 import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Select;
 
 public interface UsersMapper {
     @Insert("INSERT INTO users(username,password,role_id,act_id,draw_times,available_draw_times) VALUES(#{username}," +
             "#{password},#{role_id},#{act_id},#{draw_times},#{available_draw_times})")
     public void addUser(User user);
+
+//    @Select("SELECT username,password FROM users WHERE username = #{username}")
+//    public User login(User user);
 }
