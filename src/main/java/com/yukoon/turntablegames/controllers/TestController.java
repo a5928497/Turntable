627a5b9b0743bb21bf030f79f8ab7c778299a8a5
a5928497 <61722615@qq.com>
2018-05-29 18:44:50 +0800

    developing

---
+++ src/main/java/com/yukoon/turntablegames/controllers/TestController.java
@@ -0,0 +1,12 @@
+package com.yukoon.turntablegames.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class TestController {
+    @GetMapping("/test")
+    public String test() {
+        return "commons/navbar";
+    }
+}
