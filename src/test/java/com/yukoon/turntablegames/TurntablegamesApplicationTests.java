baa4434b633231ef0b712aaee5cbad541f1432a9
a5928497 <61722615@qq.com>
2018-06-06 17:16:43 +0800

    docker

--- src/test/java/com/yukoon/turntablegames/TurntablegamesApplicationTests.java
+++ src/test/java/com/yukoon/turntablegames/TurntablegamesApplicationTests.java
@@ -17,21 +17,8 @@ import java.util.List;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class TurntablegamesApplicationTests {
-	@Autowired
-	private DrawService drawService;
-	@Autowired
-	private AwardInfoService awardInfoService;
-	@Autowired
-	private UsersMapper usersMapper;
-
 	@Test
 	public void contextLoads() throws JsonProcessingException {
-		User u1 = new User().setUsername("feili1").setPassword("123").setRole_id(1).setAct_id(2).setDraw_times(5).setAvailable_draw_times(5);
-		User u2 = new User().setUsername("feili2").setPassword("123").setRole_id(1).setAct_id(2).setDraw_times(5).setAvailable_draw_times(5);
-		List<User> users = new ArrayList<>();
-		users.add(u1);
-		users.add(u2);
-		usersMapper.insertAll(users);
 	}
 
 }
